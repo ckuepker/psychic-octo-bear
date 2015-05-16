268ff546036f24359ebb532e730042f5211f29c8
Christoph Küpker <christoph.kuepker@googlemail.com>
2015-05-16 21:20:07 +0200

    Farbenwahl beim Dummy-Agenten ist komplett zufällig

--- IntSys_JADE_Project/src/de/unioldenburg/jade/maumau/agents/Player.java
+++ IntSys_JADE_Project/src/de/unioldenburg/jade/maumau/agents/Player.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 
 import de.unioldenburg.jade.behaviours.WaitForMessageBehaviour;
 import de.unioldenburg.jade.maumau.SelectedCard;
+import java.util.Random;
 
 /**
  * 
@@ -237,35 +238,11 @@ public class Player extends Agent {
 	 * @author Armin Pistoor
 	 * @return the wishing color
 	 */
-	//Optimierungsbedarf
 	private String getWishingColor() {
-		int clubs = 0; //Kreuzkarten
-		int spades = 0; //Pikkarten
-		int hearts = 0; //Herzkarten
-		int diamonds = 0; //Karokarten
-		for (String card : this.handCards) {
-			if (card.charAt(1) != 'B')
-				if (card.charAt(0) == 'K') {
-					clubs++;
-				} else if (card.charAt(0) == 'P') {
-					spades++;
-				} else if (card.charAt(0) == 'H') {
-					hearts++;
-				} else if (card.charAt(0) == 'C') {
-					diamonds++;
-				}
-		}
-		for (int i = handCards.size(); i > 0; i--) {
-			if (clubs == i) {
-				return "K";
-			} else if (spades == i) {
-				return "P";
-			} else if (hearts == i) {
-				return "H";
-			} else if (diamonds == i) {
-				return "C";
-			}
-		}
-		return null;
+            String[] colors = {
+                "K","P","C","H"
+            };
+            Random r = new Random();
+            return colors[r.nextInt(4)];
 	}
 }
