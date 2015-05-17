0d1eadb33a05ff0c1bed9c5e47d0d837a15f4c48
Christoph Küpker <christoph.kuepker@googlemail.com>
2015-05-17 22:30:19 +0200

    Nachrichtenkonstanten in Player belassen

--- IntSys_JADE_Project/src/de/unioldenburg/jade/maumau/behaviours/MauMauPlayerBehaviour.java
+++ IntSys_JADE_Project/src/de/unioldenburg/jade/maumau/behaviours/MauMauPlayerBehaviour.java
@@ -2,6 +2,7 @@ package de.unioldenburg.jade.maumau.behaviours;
 
 import jade.lang.acl.ACLMessage;
 import de.unioldenburg.jade.behaviours.WaitForMessageBehaviour;
+import de.unioldenburg.jade.maumau.agents.Player;
 
 /**
  * @author ckuepker
@@ -14,24 +15,17 @@ public abstract class MauMauPlayerBehaviour extends WaitForMessageBehaviour {
 	 */
 	private static final long serialVersionUID = 2932285930989468403L;
 	
-	public static final String DISTRIBUTE_CARD_MESSAGE_CONTENT = "distCard",
-			NEXT_MESSAGE_CONTENT = "next",
-			GAMEOVER_MESSAGE_CONTENT = "gameover",
-			NEXT_EXECUTE_CARD_MESSAGE_CONTENT = "nextExec",
-			NEXT_WISHED_CARD_MESSAGE_CONTENT = "wished",
-			PLAYED_CARD_MESSAGE_CONTENT_PREFIX = "played";
-
 	@Override
 	public void handleMessage(ACLMessage msg) {
-		if (msg.getContent().startsWith(DISTRIBUTE_CARD_MESSAGE_CONTENT)) {
+		if (msg.getContent().startsWith(Player.DISTRIBUTE_CARD_MESSAGE_CONTENT)) {
 			this.receiveCard(msg.getContent().substring(8));
-        } else if (msg.getContent().startsWith(NEXT_EXECUTE_CARD_MESSAGE_CONTENT)) {
+        } else if (msg.getContent().startsWith(Player.NEXT_EXECUTE_CARD_MESSAGE_CONTENT)) {
             executeTurn(msg.getContent().substring(8), true);
-        } else if (msg.getContent().startsWith(NEXT_MESSAGE_CONTENT)) {
+        } else if (msg.getContent().startsWith(Player.NEXT_MESSAGE_CONTENT)) {
             executeTurn(msg.getContent().substring(4), false);
-        } else if (msg.getContent().startsWith(NEXT_WISHED_CARD_MESSAGE_CONTENT)) {
+        } else if (msg.getContent().startsWith(Player.NEXT_WISHED_CARD_MESSAGE_CONTENT)) {
             executeTurn(msg.getContent().substring(6) + "0", false);
-        } else if (msg.getContent().equals(GAMEOVER_MESSAGE_CONTENT)) {
+        } else if (msg.getContent().equals(Player.GAMEOVER_MESSAGE_CONTENT)) {
         	this.shutdown();
         }
 	}
